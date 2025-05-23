b52aebbd0c666f2509edd7b7e32651ddee336c15
Mar1f <938224402@qq.com>
2025-05-23 21:24:06 +0800

    Add QueryRewriter and integrate with LoveApp

--- src/main/java/com/mar/mfagent/app/LoveApp.java
+++ src/main/java/com/mar/mfagent/app/LoveApp.java
@@ -4,6 +4,7 @@ package com.mar.mfagent.app;
 import com.mar.mfagent.advisor.MyLoggerAdvisor;
 import com.mar.mfagent.advisor.ReReadingAdvisor;
 import com.mar.mfagent.chatmemory.FileBasedChatMemory;
+import com.mar.mfagent.rag.QueryRewriter;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -95,6 +96,9 @@ public class LoveApp {
 
     @Resource
     private VectorStore pgVectorVectorStore;
+
+    @Resource
+    private QueryRewriter queryRewriter;
     /**
      * 和 RAG知识库对话
      * @param message
@@ -102,9 +106,14 @@ public class LoveApp {
      * @return
      */
     public String doChatWithRag(String message, String chatId) {
+        // 查询重写
+        String rewriteMessage = queryRewriter.doQueryRewrite(message);
+
         ChatResponse chatResponse = chatClient
                 .prompt()
-                .user(message)
+//                .user(message)
+                // 使用改写后的查询
+                .user(rewriteMessage)
                 .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
                 // 开启日志，便于观察效果
