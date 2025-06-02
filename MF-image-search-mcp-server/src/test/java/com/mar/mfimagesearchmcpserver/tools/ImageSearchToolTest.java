ddcda82da95b39ae77de1ccb5a1628dbba223c77
Mar1f <938224402@qq.com>
2025-06-02 17:31:06 +0800

    Add ImageSearchTool, config, and tests for web image search

---
+++ MF-image-search-mcp-server/src/test/java/com/mar/mfimagesearchmcpserver/tools/ImageSearchToolTest.java
@@ -0,0 +1,24 @@
+package com.mar.mfimagesearchmcpserver.tools;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description；
+ * @author:mar1
+ * @data:2025/05/29
+ **/
+@SpringBootTest
+class ImageSearchToolTest {
+
+    @Resource
+    private ImageSearchTool imageSearchTool;
+
+    @Test
+    void searchImage() {
+        String result = imageSearchTool.searchImage("computer");
+        Assertions.assertNotNull(result);
+    }
+}
