2ac42f3867ddd55b660111cbe9efc5e2805d5821
Mar1f <938224402@qq.com>
2025-06-03 20:05:31 +0800

    Add TerminateTool, ToolCallAgent, MfManus, and ReActAgent for enhanced agent functionality

--- src/main/java/com/mar/mfagent/MfAgentApplication.java
+++ src/main/java/com/mar/mfagent/MfAgentApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication(exclude = {PgVectorStoreAutoConfiguration.class, DataSourceAutoConfiguration.class})
-@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
+@SpringBootApplication(exclude = {PgVectorStoreAutoConfiguration.class, DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
 public class MfAgentApplication {
 
 
